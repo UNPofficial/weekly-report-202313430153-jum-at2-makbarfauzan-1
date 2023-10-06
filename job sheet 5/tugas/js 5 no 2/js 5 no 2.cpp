43734a9aa98533368849eb1f1711c32f97e059cf
makbarfauzan <144658778+makbarfauzan@users.noreply.github.com>
2023-10-06 13:49:44 +0700

    Add files via upload

---
+++ job sheet 5/tugas/js 5 no 2/js 5 no 2.cpp
@@ -0,0 +1,25 @@
+// Created by 23343075_Muhamad Akbar Fauzan
+// Program program TOSERBA
+
+#include <stdio.h>
+
+int main () {
+	// total pembelian
+	double tp;
+	
+	printf("=====TOSERBA=====");
+	printf("\n\nKetikkan total belanja : Rp. ");
+	scanf("%lf", &tp);
+	
+	if (tp <= 75000){
+		printf("Selamat anda mendapatkan diskon 5%%");
+	}
+	else if (tp > 75000, tp <= 125000){
+		printf("Selamat anda mendapatkan diskon 15%%");
+	}
+	else {
+		printf("Selamat anda mendapatkan diskon 25%% dan cashback Rp. 5000");
+	}
+	
+	return 0;
+}
