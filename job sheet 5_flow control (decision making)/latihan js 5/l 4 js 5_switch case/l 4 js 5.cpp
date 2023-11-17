1b184a24114d3c8b2f031bf28a19f9bf988a9e0b
makbarfauzan <144658778+makbarfauzan@users.noreply.github.com>
2023-11-17 09:55:30 +0700

    Add files via upload

---
+++ job sheet 5_flow control (decision making)/latihan js 5/l 4 js 5_switch case/l 4 js 5.cpp
@@ -0,0 +1,32 @@
+// created by 23343975_Muhamad Akbar Fauzan
+// latihan 4 js 5 : switch case
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    char grade;
+
+    printf("Inputkan grade: ");
+    scanf("%c", &grade);
+
+    switch (toupper(grade)){
+        case 'A':
+            printf("Luar biasa!\n");
+            break;
+        case 'B':
+        case 'C':
+            printf("Bagus!\n");
+            break;
+        case 'D':
+            printf("Anda lulus\n");
+            break;
+        case 'E':
+        case 'F':
+            printf("Anda remidi\n");
+            break;
+        default:
+            printf("Grade Salah!\n");
+    }
+    return 0;
+}
---
+++ job sheet 5_flow control (decision making)/latihan js 5/l 5 js 5_percabangan operator ternary/l 5 js 5.cpp
@@ -0,0 +1,17 @@
+// created by 23343975_Muhamad Akbar Fauzan
+// latihan 5 js 5 : percabangan operator ternary
+
+#include <stdio.h>
+
+int main(){
+
+    int jawaban;
+
+    printf("Berapakah hasil 3+4?\n");
+    printf("jawab> ");
+    scanf("%d", &jawaban);
+
+    printf("Jawaban anda: %s\n", (jawaban == 7) ? "Benar" : "Salah");
+    
+	return 0;
+}
