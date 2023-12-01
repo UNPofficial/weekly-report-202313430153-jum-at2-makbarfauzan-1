0899a8c6df35b10274df22cbc4e0825a09c31e9e
makbarfauzan <144658778+makbarfauzan@users.noreply.github.com>
2023-12-01 09:33:10 +0700

    Add files via upload

---
+++ job sheet 3_variabel, konstanta, dan tipe data/latihan js 3/l 4 js 3_konstanta define/l 4 js 3.c
@@ -0,0 +1,15 @@
+// created by 23343975_Muhamad Akbar Fauzan
+// latihan 4 js 3 : konstanta define
+
+#include <stdio.h>
+#define SEPULUH 10
+#define VERSI 4.5
+#define JENIS_KELAMIN 'L'
+
+int main(){
+	printf("isi konstanta SEPULUH adalah %i\n", SEPULUH);
+	printf("isi konstanta VERSI adalah %.1f\n", VERSI);
+	printf("isi konstanta JENIS_KELAMIN adalah %c\n", JENIS_KELAMIN);
+	
+	return 0;
+}
