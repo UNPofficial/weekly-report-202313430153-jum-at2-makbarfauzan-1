b4f0680997e826844401c4dff8a17a6746fac15d
makbarfauzan <144658778+makbarfauzan@users.noreply.github.com>
2023-12-01 10:02:43 +0700

    Add files via upload

---
+++ job sheet 4_operator/tugas js 4/t js 4 no 4_rental film/t js 4 no 4.c
@@ -0,0 +1,22 @@
+// Created by 23343075_Muhamad Akbar fauzan
+// tugas js 4 no 4 : program rental film
+
+#include <stdio.h>
+
+int main () {
+	// satu jam, durasi jam, total harga
+	int sj = 15000;
+	int dj, th;
+	
+	
+	printf("Ketikkan durasi jam menonton	: ");
+	scanf("%i", &dj);
+	
+	th = sj + (dj * 0.5 * sj) - 7500;
+	
+	printf("\n");
+	printf("Total harga yg harus dibayarkan	: Rp. %i ", th);
+	printf("\n");                               
+	                                             
+	return 0;	
+}
