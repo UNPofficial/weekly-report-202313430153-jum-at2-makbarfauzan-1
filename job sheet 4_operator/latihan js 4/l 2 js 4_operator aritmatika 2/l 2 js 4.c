5c77b2d78c233aff67960c09b25a475bb13eb3a0
makbarfauzan <144658778+makbarfauzan@users.noreply.github.com>
2023-12-01 09:54:57 +0700

    Add files via upload

---
+++ job sheet 4_operator/latihan js 4/l 2 js 4_operator aritmatika 2/l 2 js 4.c
@@ -0,0 +1,23 @@
+// created by 23343975_Muhamad Akbar Fauzan
+// latihan 2 js 4 : operator aritmatika 2
+
+#include <stdio.h>
+
+int main(){
+
+    int a, b;
+
+    printf("Inputkan nilai a: ");
+    scanf("%i", &a);
+
+    printf("Inputkan nilai b: ");
+    scanf("%i", &b);
+
+    printf("Hasil a + b: %i\n", a + b);
+    printf("Hasil a - b: %i\n", a - b);
+    printf("Hasil a * b: %i\n", a * b);
+    printf("Hasil a / b: %i\n", a / b);
+    printf("Hasil a %% b: %i\n", a % b);
+	
+	return 0;
+}
