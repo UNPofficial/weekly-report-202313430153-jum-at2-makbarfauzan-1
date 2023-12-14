ca6738de5b7cde62fb6dc297171264346bbe6694
makbarfauzan <144658778+makbarfauzan@users.noreply.github.com>
2023-12-14 10:16:05 +0700

    Add files via upload

---
+++ job sheet 04_operator/latihan js 4/l 15 js 4_operator increment dan decrement/l 15 js 4.c
@@ -0,0 +1,30 @@
+// created by 23343975_Muhamad Akbar Fauzan
+// latihan 15 js 4 : operator increment dan decrement 
+
+#include <stdio.h>
+
+int main(){
+    int a = 4;
+
+    // increment a
+    a++;
+
+    printf("a++ = %d\n", a);
+
+    // increment a
+    ++a;
+
+    printf("++a = %d\n", a);
+
+    // decrement a
+    a--;
+
+    printf("a-- = %d\n", a);
+
+    // decrement a
+    --a;
+
+    printf("--a = %d\n", a);
+    
+    return 0;
+}
